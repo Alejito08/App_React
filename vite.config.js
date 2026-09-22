d59de68deb149b708db913c1bbe904a490b47138
Alejito08 <alejandrovallegraciano2008@gmail.com>
2026-09-22 06:45:06 -0500

    Configurar base path para GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
-}) 
+  plugins: [react()],
+  base: '/App_React/', // Reemplaza 'App_React' si tu repositorio se llama diferente
+})
