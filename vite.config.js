c85998d352d84c90f7766e250287a1d870228c8c
Alejito08 <alejandrovallegraciano2008@gmail.com>
2026-09-22 06:48:05 -0500

    Configurar despliegue automático con GitHub Actions

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/App_React/', // Reemplaza 'App_React' si tu repositorio se llama diferente
+  base: '/App_React/',
 })
